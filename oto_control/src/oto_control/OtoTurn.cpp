beea359b8a67a4b146573fa3397c33af5c704c5c
chda6313 <chda6313@colorado.edu>
2018-03-21 17:08:01 -0600

    moving inits of variables to proper places, rough turn with imu, fine with ir data

--- oto_control/src/oto_control/OtoTurn.cpp
+++ oto_control/src/oto_control/OtoTurn.cpp
@@ -19,7 +19,9 @@ bool OtoController::TurnState::initialize(OtoController* controller){
     parent_controller = controller;
     init_yaw = parent_controller->yaw;
     final_yaw = parent_controller->yaw - (M_PI / 2);
-		
+	infinity_threshold = parent_controller->cfg.min_turn_distance; //really just a value to not do math with absurdly large sensor data
+	parallel_threshold = 10; //10 cm for now, replace once testing done
+	
     bool success = true;
     ROS_INFO("Initialized Turn State");
     return success;
@@ -27,45 +29,39 @@ bool OtoController::TurnState::initialize(OtoController* controller){
 
 void OtoController::TurnState::turn(){
 	init_yaw = parent_controller->turn_init_yaw;
-    final_yaw = parent_controller->turn_init_yaw - (M_PI / 2);
+    final_yaw = parent_controller->turn_init_yaw - deg_to_rad(80);
 	sensor_interpret();
 }
 
 void OtoController::TurnState::sensor_interpret(){
-	//option 1: use yaw, check if we have turned 90 degrees yet.
+	//check imu for a rough estimate of our progress through turn
 	if (parent_controller->yaw <= final_yaw){
-		parent_controller->state = CRUISE;
-	}
-	//option 2: use ir sensors, turn until paralell to a wall (withen error bound turh_threshold):
-	double infinity_threshold, parallel_threshold;
-
-	infinity_threshold = parent_controller->cfg.min_turn_distance; //really just a value to not do math with absurdly large sensor data
-	parallel_threshold = 10; //10 cm for now, replace once testing done
-
-	if ((parent_controller->distance_plant_f <= infinity_threshold) && (parent_controller->distance_plant_r <= infinity_threshold)){
-		if (abs(parent_controller->distance_plant_f - parent_controller->distance_plant_r) < parallel_threshold){
-			//we are parallel(ish) to a wall
-			parent_controller->state = CRUISE;
+		
+		//start checking ir sensor for fine angle measurement
+		if ((parent_controller->distance_plant_f <= infinity_threshold) && (parent_controller->distance_plant_r <= infinity_threshold)){
+			if (abs(parent_controller->distance_plant_f - parent_controller->distance_plant_r) < parallel_threshold){
+				//we are parallel(ish) to a wall
+				parent_controller->state = CRUISE;
+				ROS_INFO("ir trigger");
 		}
 	}
-	//note option 2 has a problem if we turn too fast, rear sensor distance reading will grow to inf, mitigate with following:
+		
+	}
+	
+	//turning control, could modify to use pid if needed
 	if (parent_controller->distance_plant_r > infinity_threshold){
 		//TURN SLOWER not really sure what to modify here
 		
 		motor_command.joint_name = "steering";
-		motor_command.position = deg_to_rad(-15);////////// //slower
+		motor_command.position = deg_to_rad(-15);//slower when needed
 		parent_controller->publish_motor_command(motor_command);
 		
-    ROS_INFO("turning at 15/45");
 	}
 	else{
 		motor_command.joint_name = "steering";
-		motor_command.position = deg_to_rad(-45);////////// //slower
+		motor_command.position = deg_to_rad(-30);//default case is fast
 		parent_controller->publish_motor_command(motor_command);
-	
-		ROS_INFO("turning at 45/45");}
-
-
+	}
 }
 
 OtoController::TurnState::~TurnState()
